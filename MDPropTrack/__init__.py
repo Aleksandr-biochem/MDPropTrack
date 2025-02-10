3d4be3f0491deed818e8a1a338f92284f249bde7
Aleks Kovalenko <ikovalenko239@gmail.com>
2025-02-10 16:42:07 +0000

    separated propery_calculators.py

--- MDPropTrack/__init__.py
+++ MDPropTrack/__init__.py
@@ -1,2 +1,2 @@
 from .analysis import PropertyAnalyser
-from .analysis import LipidPropertyCalculator, ProteinPropertyCalculator
+from .property_calculators import LipidPropertyCalculator, ProteinPropertyCalculator
